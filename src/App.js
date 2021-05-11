eef5f80fd31aaa51a14ab07fd8d69f5c6dd33dca
Ro5hi <slen.detablan@gmail.com>
2021-05-11 12:24:18 -0600

    added hero, imported to app, added config

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Hero from './components/Hero.js';
 
 function App() {
   return (
     <div className="">
-
+      <Hero />
     </div>
   );
 }
---
+++ src/components/Hero.js
@@ -0,0 +1,11 @@
+const Hero = () => {
+    return (
+        <div className="bg-white h-screen flex flex-col justify-center items-center">
+            <div className="text-4xl">
+                Hero
+            </div>
+        </div>
+    );
+}
+
+export default Hero;
