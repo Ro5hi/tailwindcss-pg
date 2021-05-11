62df2934233d4241d73a2d4fddbec305f3455c98
Ro5hi <slen.detablan@gmail.com>
2021-05-11 12:29:19 -0600

    updated root config json files

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: 'media',
+  theme: {
+    extend: {
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+        '3xl': '1600px',
+        '4xl': '1920px',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
