97eaa0e86dcc3960108f916690e7578cc888bf80
Ro5hi <slen.detablan@gmail.com>
2021-05-11 12:49:27 -0600

    updated hero, adding info section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Hero = () => {
     return (
-        <div className="bg-white h-screen flex flex-col justify-center items-center">
-            <div className="text-4xl">
+        <div className="bg-white h-screen flex justify-center items-center">
+            <div className="text-9xl">
                 Hero
             </div>
         </div>
