f7d9884b0a88aaeae3a8b0518a7367620029f0f0
Ro5hi <slen.detablan@gmail.com>
2021-05-12 16:18:58 -0600

    added info and exported to app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import React from 'react';
 import Hero from './components/Hero.js';
 import Nav from './components/Nav.js';
+import Info from './components/Info.js';
 
 function App() {
   return (
     <div className="app">
       <Nav />
       <Hero />
+      <Info />
     </div>
   );
 }
--- src/components/Info.js
+++ src/components/Info.js
@@ -2,8 +2,27 @@ import React from 'react';
 
 const Info = () => {
     return(
-        <div className="">
-            Info
+        <div className="bg-black h-screen flex justify-center items-center">
+            <div className="grid grid-cols-3 gap-6">
+                <div class="bg-white h-60 w-96 col-span-2 border-r-8 border-b-8 border-blue-300">
+                    <p className="text-black text-2xl text-center">1</p>
+                </div>
+                <div className="p-9 text-m text-white">
+                    Info
+                </div>
+                <div class="bg-white h-60 w-96 col-span-2 border-r-8 border-b-8 border-blue-300">
+                    <p className="text-black text-2xl text-center">2</p>
+                </div>
+                <div className="p-9 text-white">
+                    Info
+                </div>
+                <div class="bg-white h-60 w-96 col-span-2 border-r-8 border-b-8 border-blue-300">
+                    <p className="text-black text-2xl text-center">3</p>
+                </div>
+                <div className="p-9 text-white">
+                    Info
+                </div>
+            </div>
         </div>
     );
 }
