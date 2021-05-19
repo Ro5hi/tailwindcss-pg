46c8a0c07fe41251ee7d2d15887acae175a3ee1f
Ro5hi <slen.detablan@gmail.com>
2021-05-19 15:26:36 -0600

    added logo and title to hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,8 +3,11 @@ import React from "react";
 const Hero = () => {
     return (
         <div className="bg-white h-screen flex justify-center items-center">
-            <div className="text-9xl">
-                Hero
+            <div className="">
+                <img src="/logo.png" alt="graphic"></img>
+                <div className="text-6xl text-center italic text-blue-700 text-opacity-70 font-bold">
+                    ONBOARD
+                </div>
             </div>
         </div>
     );
