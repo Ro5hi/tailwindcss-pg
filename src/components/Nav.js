854487b694cdf80365e099b7f80f8fbf12b0504d
Ro5hi <slen.detablan@gmail.com>
2021-05-20 15:05:42 -0600

    added link smooth items

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,15 @@
+import { HashLink as Link } from 'react-router-hash-link';
+
 const Nav = () => {
     return (
-        <div className="flex fixed w-screen h-16 justify-between items-center bg-yellow-300">
-            <div className="text-black text-center">
-                <div className="pr-8">
-                    <ul>Info</ul>
-                    <ul>Contact</ul>
-                </div>
+        <div className="flex fixed justify-between items-center w-screen h-16 bg-yellow-300 relative">
+            <div className="text-black p-4 text-center">
+                <Link smooth to="#info" className="p-4">
+                    INFO
+                </Link>
+                <Link smooth to="#contact" className="p-4">
+                    CONTACT
+                </Link>
             </div>
         </div>
     );
