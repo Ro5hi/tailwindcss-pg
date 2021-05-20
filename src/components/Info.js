2c0077c5211e7b881ee9bd0a3bfb355fd47ef25f
Ro5hi <slen.detablan@gmail.com>
2021-05-20 14:27:26 -0600

    centering items, improving styling

--- src/components/Info.js
+++ src/components/Info.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Info = () => {
     return(
-        <div className="bg-black h-screen w-screen flex justify-center items-center">
+        <div className="bg-black h-screen w-screen flex justify-center items-center text-center">
             <div className="grid grid-cols-3 gap-6">
                 <div class="bg-white h-60 w-96 place-items-center mx-8 border-r-8 border-b-8 border-blue-300">
                     <img class="object-contain h-40" src="/1.jpg" alt="1"></img>
