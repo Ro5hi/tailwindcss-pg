c5920e0b22f7e226562689e8f833ba2b18362a68
Ro5hi <slen.detablan@gmail.com>
2021-05-20 14:55:09 -0600

    select-none on text elements

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Contact = () => {
     return (
-        <div className="flex bg-gray-800 h-screen w-screen space-x-8 justify-center items-center">
+        <div className="select-none flex bg-gray-800 h-screen w-screen space-x-8 justify-center items-center">
             <p className="text-white text-2xl">Need support? <br/>Contact us anytime.</p>
             <div className="w-1/16">
                 <input type="text" className="h-64 w-72 text-center" placeholder="Type your message here."></input>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,7 @@ const Hero = () => {
         <div className="bg-white h-screen w-screen flex justify-center items-center">
             <div className="">
                 <img src="/easternwaves.svg" alt="graphic"></img>
-                <div className="text-6xl text-center italic text-opacity-70 font-bold">
+                <div className="select-none text-6xl text-center italic text-opacity-70 font-bold">
                     ONBOARD
                 </div>
             </div>
