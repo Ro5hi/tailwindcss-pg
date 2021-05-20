e88ea7276a26fcb95a7025ff84df49beb90a67e0
Ro5hi <slen.detablan@gmail.com>
2021-05-20 15:23:04 -0600

    added imports and suspense, rendered components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
+import React, { Suspense } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
-import React from 'react';
+import Home from './components/Home'
 import Hero from './components/Hero.js';
 import Nav from './components/Nav.js';
 import Info from './components/Info.js';
@@ -8,17 +9,22 @@ import Contact from './components/Contact.js';
 import Footer from './components/Footer.js';
 
 function App() {
+
+  const Home = React.lazy(() => import('./components/Home'))
+  const Info = React.lazy(() => import('./components/Info'))
+  const Contact = React.lazy(() => import('./components/Contact'))
+
   return (
     <div className="w-screen">
+      <Suspense fallback={""}>
       <BrowserRouter>
         <Switch>
-          <Route component={Nav} />
-          <Route component={Hero} />
-          <Route component={Info} />
-          <Route component={Contact} />
-          <Route component={Footer} />
+          <Route component={Home} path='/' />
+          <Route component={Info} path='/info' />
+          <Route component={Contact} path='/contact' />
         </Switch>
       </BrowserRouter>
+      </Suspense>
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+import Hero from './Hero';
+import Nav from './Nav';
+import Info from './Info';
+import Contact from './Contact';
+import Footer from './Footer';
+
+const Home = () => {
+    return (
+        <div className="">
+            <Nav />
+            <Hero />
+            <Info />
+            <Contact />
+            <Footer />
+        </div>
+    );
+}
+
+export default Home;
