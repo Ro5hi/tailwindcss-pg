8c76d6387422d5a715111176f5bbe99eecc7c7fe
Ro5hi <slen.detablan@gmail.com>
2021-05-21 13:36:51 -0600

    changed description, added slogan, removed unused class

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Contact = () => {
     return (
         <div className="select-none flex bg-gray-800 h-screen space-x-8 justify-center items-center" id="contact">
-            <p className="text-white text-2xl">Need support? <br/>Contact us anytime.</p>
+            <p className="text-white text-2xl">Changing the world? <br/>Let us know.</p>
             <div className="w-1/16">
                 <input type="text" className="h-64 w-72 text-center focus:outline-none" placeholder="Type your message here."></input>
                 <div className="relative bottom-0 bg-green-400 text-white text-center">
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,10 +3,13 @@ import React from "react";
 const Hero = () => {
     return (
         <div className="bg-white h-screen flex justify-center items-center">
-            <div className="">
+            <div className="object-fill">
                 <img src="/easternwaves.svg" alt="graphic"></img>
                 <div className="select-none text-6xl text-center italic text-opacity-70 font-bold">
                     ONBOARD
+                    <p className="text-center text-lg p-4 italic text-opacity-35 font-light">
+                        Using old technology to impact communities.
+                    </p>
                 </div>
             </div>
         </div>
