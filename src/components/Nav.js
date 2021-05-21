287c7624075c756c854a7dbe54e68487bb5157b1
Ro5hi <slen.detablan@gmail.com>
2021-05-21 13:15:04 -0600

    fixed nav on screen

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,14 +2,16 @@ import { HashLink as Link } from 'react-router-hash-link';
 
 const Nav = () => {
     return (
-        <div className="flex fixed justify-between items-center w-screen h-16 bg-yellow-300 relative">
-            <div className="text-black mx-auto p-4 text-center">
-                <Link smooth to="#info" className="p-4 hover:bg-blue-400 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-25">
-                    INFO
-                </Link>
-                <Link smooth to="#contact" className="p-4 hover:bg-blue-400 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-25">
-                    CONTACT
-                </Link>
+        <div className="flex flex-col fixed w-screen">
+            <div className="w-full z-50 justify-between items-center h-16 bg-yellow-300 relative">
+                <div className="sticky text-black mx-auto p-4 text-center">
+                    <Link smooth to="#info" className="p-4 hover:bg-blue-400 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-25">
+                        INFO
+                    </Link>
+                    <Link smooth to="#contact" className="p-4 hover:bg-blue-400 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-25">
+                        CONTACT
+                    </Link>
+                </div>
             </div>
         </div>
     );
