008abec35053b59dcfe46989d0b4063d6a2a35fa
Ro5hi <slen.detablan@gmail.com>
2021-05-21 12:45:04 -0600

    removed outlin

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,7 @@ const Contact = () => {
         <div className="select-none flex bg-gray-800 h-screen space-x-8 justify-center items-center" id="contact">
             <p className="text-white text-2xl">Need support? <br/>Contact us anytime.</p>
             <div className="w-1/16">
-                <input type="text" className="h-64 w-72 text-center" placeholder="Type your message here."></input>
+                <input type="text" className="h-64 w-72 text-center focus:outline-none" placeholder="Type your message here."></input>
                 <div className="relative bottom-0 bg-green-400 text-white text-center">
                     <button class="w-full hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50">Send Message</button>
                 </div>
