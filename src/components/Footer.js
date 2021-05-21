4b178f098ffe28f3b22aa732b94486b09b445a52
Ro5hi <slen.detablan@gmail.com>
2021-05-21 12:45:39 -0600

    select-none on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className="flex bg-yellow-300 w-screen h-12 justify-center items-center">
+        <div className="select-none flex bg-yellow-300 w-screen h-12 justify-center items-center">
             <div className="text-center text-black">
                 Onboard ™ 2021
             </div>
