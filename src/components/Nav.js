261be8ba850a42f2d7444f0c1f9d6441216554a5
Ro5hi <slen.detablan@gmail.com>
2021-05-21 13:44:12 -0600

    changed focus ring to blue

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,10 +5,10 @@ const Nav = () => {
         <div className="flex flex-col fixed w-screen">
             <div className="w-full z-50 justify-between items-center h-16 bg-yellow-300 relative">
                 <div className="sticky text-black mx-auto p-4 text-center">
-                    <Link smooth to="#info" className="p-4 hover:bg-blue-400 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-25">
+                    <Link smooth to="#info" className="p-4 hover:bg-blue-400 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-25">
                         INFO
                     </Link>
-                    <Link smooth to="#contact" className="p-4 hover:bg-blue-400 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-25">
+                    <Link smooth to="#contact" className="p-4 hover:bg-blue-400 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-25">
                         CONTACT
                     </Link>
                 </div>
