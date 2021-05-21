068a1e3793f34af98d51428b10e6c1a164023dd8
Ro5hi <slen.detablan@gmail.com>
2021-05-21 12:05:28 -0600

    styling info, centered nav items, fixing w-screen

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Hero = () => {
     return (
-        <div className="bg-white h-screen w-screen flex justify-center items-center">
+        <div className="bg-white h-screen flex justify-center items-center">
             <div className="">
                 <img src="/easternwaves.svg" alt="graphic"></img>
                 <div className="select-none text-6xl text-center italic text-opacity-70 font-bold">
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,7 @@ import { HashLink as Link } from 'react-router-hash-link';
 const Nav = () => {
     return (
         <div className="flex fixed justify-between items-center w-screen h-16 bg-yellow-300 relative">
-            <div className="text-black p-4 text-center">
+            <div className="text-black mx-auto p-4 text-center">
                 <Link smooth to="#info" className="p-4">
                     INFO
                 </Link>
