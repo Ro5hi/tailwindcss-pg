2e1d21a32478ad3c8cd8f9dde5073dd45bb87b62
Ro5hi <slen.detablan@gmail.com>
2021-05-27 15:12:01 -0600

    testing component rendering

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import { shallow, mount } from 'enzyme';
+import { HashLink as Link } from 'react-router-hash-link';
+import App from './App';
 import Nav from './components/Nav';
 import Hero from './components/Hero';
 import Info from './components/Info';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
-describe("Renders components", () => {
-  it("renders App component"), () => {
-    shallow(<App />)
-  }
+describe("component rendering", () => {
+  it("renders App component", () => {
+    shallow(<App />);
+  });
+  it("renders Nav component with Nav items", () => {
+    shallow(<Nav />)
+  })
 })
