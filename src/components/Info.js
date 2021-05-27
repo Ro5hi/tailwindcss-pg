e2397481fd6e7ccf28df02cc2c95fac61c652cde
Ro5hi <slen.detablan@gmail.com>
2021-05-27 17:37:08 -0600

    updated mobile responsiveness

--- src/components/Info.js
+++ src/components/Info.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Info = () => {
     return(
         <div className="flex flex-wrap justify-center place-items-center bg-black w-screen h-screen text-center" id="info">
-            <div className="grid grid-cols-3 gap-2 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3 xl:grid-cols-3">
+            <div className="grid grid-cols-3 gap-2 tn:flex xsm:flex sm:flex tn:grid-cols-1 xsm:grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3 xl:grid-cols-3">
                 <div class="bg-white space-y-8 h-72 w-72 mx-8 border-r-8 border-b-8 border-blue-300 transition duration-500 ease-in-out transform outline-none hover:-translate-y-1 hover:scale-110">
                     <img class="h-40 mx-auto" src="/1.jpg" alt="1"></img>
                     <div className="select-none text-center p-2 mx-8 text-m text-black">
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
   theme: {
     extend: {
       screens: {
+        'tn': '240px',
         'xsm': '411px',
         'sm': '640px',
         'md': '768px',
