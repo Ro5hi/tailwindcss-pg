ea3cdd60e8a52c0cdac06cef339504ee0dbbbd01
Ro5hi <slen.detablan@gmail.com>
2021-05-27 13:22:50 -0600

    removed extra fixed line, removed overlapping img bug

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,7 @@ import { HashLink as Link } from 'react-router-hash-link';
 
 const Nav = () => {
     return (
-        <div className="flex flex-col fixed">
+        <div className="flex flex-col">
             <div className="fixed w-full z-50 justify-between items-center h-16 bg-yellow-300">
                 <div className="text-black mx-auto p-4 text-center">
                     <Link smooth to="#info" className="p-4 hover:bg-blue-400 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-25">
