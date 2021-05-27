fba3cd0bacdcad974fc3ecd7140694fb942165c0
Ro5hi <slen.detablan@gmail.com>
2021-05-27 16:20:09 -0600

    set purge to false

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React, { Suspense } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
-import Home from './components/Home'
-import Hero from './components/Hero.js';
-import Nav from './components/Nav.js';
-import Info from './components/Info.js';
-import Contact from './components/Contact.js';
-import Footer from './components/Footer.js';
 
 function App() {
 
